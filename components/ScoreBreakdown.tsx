77273faaa7d44207625b93b730b5d9c63acd0161
afkhan-fardeen <afkhan.fardeen@gmail.com>
2026-02-09 13:14:12 +0300

    build error fix

--- components/ScoreBreakdown.tsx
+++ components/ScoreBreakdown.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { ScoreBreakdown } from '@/types/report'
+import type { ScoreBreakdown } from '@/types/report'
 
 interface ScoreBreakdownProps {
   breakdown: ScoreBreakdown
